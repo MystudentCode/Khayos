a9555a00f378c6f64fe1b62a1adbdd88be6447b9
MystudentCode <sumant.zade2@gmail.com>
2026-03-24 11:16:02 +0530

    #CreatingModelsTables(Category)

---
+++ YumBlazor/Data/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter valid Name")]
+        public string Name { get; set; }
+    }
+}

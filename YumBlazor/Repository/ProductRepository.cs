2465afff6e223b9bd9c9fd1fb13fd028b47b7766
MystudentCode <sumant.zade2@gmail.com>
2026-03-30 17:18:12 +0530

    #dev/115_DeleteProductInAction

--- YumBlazor/Repository/ProductRepository.cs
+++ YumBlazor/Repository/ProductRepository.cs
@@ -8,10 +8,12 @@ namespace YumBlazor.Repository
     {
 
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductRepository(ApplicationDbContext db)
+        public ProductRepository(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<Product> CreateAsync(Product obj)
@@ -24,6 +26,11 @@ namespace YumBlazor.Repository
         public async Task<bool> DeleteAsync(int id)
         {
             var obj=await _db.Product.FirstOrDefaultAsync(u => u.Id == id);
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('/')); 
+            if(File.Exists(imagePath))
+            {
+                File.Delete(imagePath);
+            }
             if(obj!=null)
             {
                 _db.Product.Remove(obj);
